55ccad36083d6437f7458f8f744bcc8a81a40376
Ahmetcan Gürel <gurelahmetcan@gmail.com>
2024-01-27 14:33:11 +0100

    drag and movement

---
+++ Assets/Scripts/DragThrow.cs
@@ -0,0 +1,23 @@
+using System;
+using UnityEngine;
+using System.Collections.Generic;
+ 
+public class DragThrow : MonoBehaviour
+{
+    private Vector3 mousePosition;
+    private float posY;
+    public LayerMask layerMask;
+    private void Start()
+    {
+        posY = 1.5f;
+    }
+
+    private void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out RaycastHit raycastHit, float.MaxValue, layerMask))
+        {
+            transform.position = new Vector3(raycastHit.point.x, posY, raycastHit.point.z);
+        }
+    }
+}
