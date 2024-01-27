0a4b2d6a7f98aa7c160cd57e7906238411d2c7d7
Ahmetcan Gürel <gurelahmetcan@gmail.com>
2024-01-27 14:35:08 +0100

    y position update

--- Assets/Scripts/DragThrow.cs
+++ Assets/Scripts/DragThrow.cs
@@ -17,7 +17,7 @@ public class DragThrow : MonoBehaviour
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         if (Physics.Raycast(ray, out RaycastHit raycastHit, float.MaxValue, layerMask))
         {
-            transform.position = new Vector3(raycastHit.point.x, posY, raycastHit.point.z);
+            transform.position = new Vector3(raycastHit.point.x, raycastHit.point.y + 0.5f, raycastHit.point.z);
         }
     }
 }
