eaa4e9076991a506e7fb26b93ccc12b85fea5282
Stefan Gerstner <stefan.gerstner96@web.de>
2024-01-28 11:56:16 +0100

    Prefabs and interactions

---
+++ Assets/Scripts/DrawerPuzzle.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawerPuzzle : MonoBehaviour
+{
+    public GameObject[] drawers;
+    public float[] extension = new float[3];
+    public void InteractWithDrawer(int id)
+    {
+        switch (id) {
+            case 0:
+                if (extension[0] >0)
+                {                    
+                    drawers[0].transform.position -= Vector3.forward*extension[1]; 
+                    extension[0]=0f; 
+                    if (extension[1] == 0) 
+                    {
+                        extension[1]=0.6f;  
+                        drawers[1].transform.position += Vector3.forward*extension[1]; 
+                    }     
+                }
+                else
+                {
+                    extension[0]=0.6f;
+                    drawers[0].transform.position += Vector3.forward*extension[1];  
+                    if (extension[2]>0)
+                    {
+                        drawers[2].transform.position -= Vector3.forward*extension[2];    
+                        extension[2]=0f; 
+                    }
+                }
+            break;
+            case 1:
+                if (extension[1] >0)
+                {                    
+                    drawers[1].transform.position -= Vector3.forward*extension[1]; 
+                    extension[1]=0f; 
+                    if (extension[2] < 0.6f) 
+                    {
+                        extension[2]+=0.2f;  
+                        drawers[2].transform.position += Vector3.forward*extension[2]; 
+                    }
+                    
+                     
+                }
+                else
+                {
+                    extension[1]=0.6f;
+                    drawers[1].transform.position += Vector3.forward*extension[1];  
+                    if (extension[2]>0)
+                    {
+                        drawers[2].transform.position -= Vector3.forward*extension[2];    
+                        extension[2]=0f; 
+                    }
+                }
+            break;
+            case 2:
+                if (extension[2] >0)
+                {
+                    for (int i=0; id<3; id++)
+                    {
+                        drawers[i].transform.position -= Vector3.forward*extension[i];    
+                        extension[i]=0f; 
+                    }
+                }
+                else
+                {
+                    extension[2]=0.2f;
+                    drawers[2].transform.position += Vector3.forward*extension[2];  
+                    if (extension[0]==0f)
+                    {
+                        extension[0]=0.6f;
+                        drawers[0].transform.position += Vector3.forward*extension[2];  
+                    }
+                }
+            break;
+        }
+        
+        
+    }
+}
--- Assets/Scripts/Objects/WorkbenchDrawer.cs
+++ Assets/Scripts/Objects/WorkbenchDrawer.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class BrokenFurniture : InteractableObject
+public class WorkbenchDrawer : InteractableObject
 {
+    public DrawerPuzzle drawerPuzzle;
+    public int drawerID;
     protected override void TryInteraction(InteractableObject obj) {}
     protected override void TryUsage() {
-        transform.GetChild(0).GetComponent<Animator>().Play("Play");
+        drawerPuzzle.InteractWithDrawer(drawerID);
     }
 }
